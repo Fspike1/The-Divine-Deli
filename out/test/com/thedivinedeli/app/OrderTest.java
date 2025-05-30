7b146354d3b9bc00afe21e056cc2624a0fddc465
faith spikes <faithspikes3@gmail.com>
2025-05-29 18:23:36 -0700

    added receipt file and more

---
+++ out/test/com/thedivinedeli/app/OrderTest.java
@@ -0,0 +1,24 @@
+package com.thedivinedeli.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class OrderTest {
+
+
+    @Test
+    public void testAddSandwichIncreasesCartSize() {
+        Order order = new Order();
+        Sandwich sandwich = new Sandwich();
+        sandwich.setSandwichName("Testwich");
+
+        order.addSandwich(sandwich);
+
+        assertEquals(1, order.getSandwiches().size());
+        assertEquals("Testwich", order.getSandwiches().get(0).getSandwichName());
+    }
+}
+
+
